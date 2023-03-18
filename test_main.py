663e111f43a1c6aee51db6cd5c22a491d9d10c22
Taylor Brown <tbrown5@mail.immaculata.edu>
2023-03-18 16:09:30 -0400

    adding docstrings..

--- test_main.py
+++ test_main.py
@@ -5,13 +5,20 @@ Tests for my Weapons of Rust class
 from main import Weapons
 
 def test_weapon_choice():
+    """
+    Tests the weapon name is inserted correctly
+    """
     test_weapon = Weapons("Ray Gun", 4, 10000, 100)
     summary = test_weapon.weapon_choice()
     assert "Ray Gun" in summary
 
 def test_dps_calculations():
+    """
+    Tests for accurate dps calculations
+    """
     test_weapon = Weapons("Ray Gun", 4, 10000, 100)
     assert test_weapon.dps_calculations() == 1000000
 
     test_weapon2 = Weapons("Rock", 0, 1, 2)
     assert test_weapon2.dps_calculations() == 2
+    
