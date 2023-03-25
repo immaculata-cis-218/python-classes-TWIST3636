02291557d11ea024fc8dbf346c5d6fe5e1fb2c06
Taylor Brown <tbrown5@mail.immaculata.edu>
2023-03-25 17:06:43 -0400

    Subclasses!

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ class Weapons:
         States the weapon choice and tier of that weapon
         """
         return (
-            f"You have chosen to use,"
-            f"{self.__weapon_name},"
+            f"You chose a "
+            f"{self.__weapon_name}, "
             f"which is a tier {self.__weapon_tier} weapon"
         )
 
@@ -32,9 +32,89 @@ class Weapons:
         return dps
 
 
+class Pistol(Weapons):
+    """
+    Weapons of Rust that use pistol ammunition
+    """
+
+    def __init__(
+        self,
+        weapon_name,
+        weapon_tier,
+        fire_rate,
+        damage,
+        pistol_ammo=9,
+        gunpowder=5,
+        metal_frags=10,
+    ):
+        self.pistol_ammo = pistol_ammo
+        self.__gunpowder = gunpowder
+        self.__metal_frags = metal_frags
+        super().__init__(weapon_name, weapon_tier, fire_rate, damage)
+
+    def ammo_choice(self):
+        """
+        States the weapon choice summary and that this uses pistol ammo
+        """
+        weapon_summary = super().weapon_choice()
+        weapon_summary = (
+            f"{weapon_summary} and that uses "
+            f"{self.pistol_ammo}mm ammo "
+            f"which costs {self.__gunpowder} gunpowder "
+            f"and {self.__metal_frags} metal frags to craft 4 bullets"
+        )
+        return weapon_summary
+
+    def craft_stack(self):
+        """
+        Calculates the amount of resources needed to craft a stack of 128 pistol bullets
+        """
+        stack = (
+            f"{self.__gunpowder*32} gunpowder and {self.__metal_frags*32} metal frags"
+        )
+        return stack
+
+
+class Primative(Weapons):
+    """
+    Weapons of Rust that are considered primative and use arrows
+    """
+
+    def __init__(
+        self, weapon_name, weapon_tier, fire_rate, damage, arrows=2, wood=25, stones=10
+    ):
+        self.arrows = arrows
+        self.__wood = wood
+        self.__stones = stones
+        super().__init__(weapon_name, weapon_tier, fire_rate, damage)
+
+    def wooden_arrows(self):
+        """
+        States the weapon choice summary and that this uses arrows
+        """
+        arrow_summary = super().weapon_choice()
+        arrow_summary = (
+            f"{arrow_summary} and that uses "
+            f"arrows which are crafted {self.arrows} arrows "
+            f"per {self.__wood} wood and {self.__stones} stones"
+        )
+        return arrow_summary
+
+    def craft_stack_arrows(self):
+        """
+        Calculates the amount of resources needed to craft a stack of 64 arrows
+        """
+        stack = f"{self.__wood*32} wood and {self.__stones*32} stones"
+        return stack
+
+
 if __name__ == "__main__":
-    hunting_bow = Weapons("Hunting Bow", 1, 1, 30)
-    tommy = Weapons("Tommy", 2, 5, 10)
-    ak = Weapons("Ak", 3, 6, 18)
+    hunting_bow = Primative("Hunting Bow", weapon_tier=1, fire_rate=1, damage=30)
+    tommy = Pistol("Tommy", weapon_tier=2, fire_rate=5, damage=10)
+    ak = Weapons("Ak", weapon_tier=3, fire_rate=6, damage=18)
     print(ak.weapon_choice())
     print(ak.dps_calculations())
+    print(tommy.ammo_choice())
+    print(tommy.craft_stack())
+    print(hunting_bow.wooden_arrows())
+    print(hunting_bow.craft_stack_arrows())
--- test_main.py
+++ test_main.py
@@ -3,6 +3,8 @@ Tests for my Weapons of Rust class
 """
 
 from main import Weapons
+from main import Pistol
+from main import Primative
 
 def test_weapon_choice():
     """
@@ -21,4 +23,43 @@ def test_dps_calculations():
 
     test_weapon2 = Weapons("Rock", 0, 1, 2)
     assert test_weapon2.dps_calculations() == 2
-    
+
+def test_ammo_choice():
+    """
+    Tests the name of the weapon and the caliber is stated correctly 
+    """
+    test_weapon = Pistol("M9", weapon_tier=2, fire_rate=3,
+                          damage=14, pistol_ammo=45, gunpowder=10, metal_frags=20)
+    summary = test_weapon.ammo_choice()
+    assert "M9" in summary
+    assert str(45) in summary
+
+def test_craft_stack():
+    """
+    Tests for accurate resource calculations
+    """
+    test_weapon = Pistol("M9", weapon_tier=2, fire_rate=3,
+                          damage=14, pistol_ammo=45, gunpowder=10, metal_frags=20)
+    assert test_weapon.craft_stack() == f"{320} gunpowder and {640} metal frags"
+
+    test_weapon1 = Pistol("Revolver", weapon_tier=1, fire_rate=2, damage=4)
+    assert test_weapon1.craft_stack() == f"{160} gunpowder and {320} metal frags"
+
+def test_wooden_arrows():
+    """
+    Tests the name of the weapon and resources needed are reflected
+    """
+    test_weapon = Primative("Crossbow", weapon_tier=1, fire_rate=1,
+                          damage=45, arrows=4, wood=50, stones=20)
+    summary = test_weapon.wooden_arrows()
+    assert "Crossbow" in summary
+    assert str(50) in summary
+    assert str(20) in summary
+
+def test_craft_stack_arrows():
+    """
+    Tests the accurate output of resources need to craft a stack of arrows
+    """
+    test_weapon = Primative("Crossbow", weapon_tier=1, fire_rate=1,
+                          damage=45, arrows=4, wood=50, stones=20)
+    assert test_weapon.craft_stack_arrows() == f"{1600} wood and {640} stones"
